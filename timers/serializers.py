2210c7ebceb5b192c9f3728e807b6658d5c22eb0
Archietheowl <adsg1206@gmail.com>
2024-11-15 21:32:02 +0000

    timers view built and tested.

--- timers/serializers.py
+++ timers/serializers.py
@@ -1,9 +1,7 @@
 from rest_framework.serializers import ModelSerializer
 from .models import Timer
-from users.serializers import UserSerializer
 
 class TimerSerializer(ModelSerializer):
     class Meta:
         model = Timer
-        fields = ('id', 'name', 'reason', 'started', )
-        read_only_fields = ('user', 'created_at', 'updated_at') 
+        fields = '__all__'
