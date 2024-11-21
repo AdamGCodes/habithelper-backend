600fe3ab62635597b53633c2b0263a86fdf558e8
Archietheowl <adsg1206@gmail.com>
2024-11-21 00:20:21 +0000

    not sure there's any changes here

--- project/settings.py
+++ project/settings.py
@@ -133,9 +133,9 @@ AUTH_USER_MODEL = 'users.User'
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-GB'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'GMT'
 
 USE_I18N = True
 
