e69b13965bdf7bfcb450e6bb8a927502adc0fe2d
Archietheowl <adsg1206@gmail.com>
2025-07-03 15:30:26 +0100

    Addings made to habithelper model

--- habit_helpers/models.py
+++ habit_helpers/models.py
@@ -5,6 +5,9 @@ class Habit_Helper(models.Model):
     name = models.CharField(max_length=100)
     reason = models.TextField(max_length=200, null=True)
     target = models.IntegerField()
+    completions = models.IntegerField(default=0)
+    streak = models.IntegerField(default=0)
+    last_reset = models.DateField(auto_now_add=True)
     user = models.ForeignKey(
         to='users.User',
         related_name='user_habit_helper',
